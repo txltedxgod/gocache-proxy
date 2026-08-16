324cdb63644fe217fa51fb1c15dd6e0d761e0230
txltedxgod <seidoutakidzava@gmail.com>
2026-08-17 01:49:01 +0400

    init go module and proxy server scaffold

---
+++ cmd/proxy/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	listenAddr  = flag.String("listen", ":8080", "Server listen address")
+	upstreamURL = flag.String("upstream", "http://localhost:8000", "Upstream target server URL")
+	cacheTTL    = flag.Duration("ttl", 60*time.Second, "Default response cache TTL")
+	maxCapacity = flag.Int("capacity", 5000, "Maximum number of cached entries in LRU")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Printf("[gocache-proxy] Starting on %s -> Upstream: %s (TTL: %v, Capacity: %d)\n",
+		*listenAddr, *upstreamURL, *cacheTTL, *maxCapacity)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	})
+
+	server := &http.Server{
+		Addr:         *listenAddr,
+		Handler:      mux,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	log.Println("[gocache-proxy] Shutting down gracefully...")
+}
